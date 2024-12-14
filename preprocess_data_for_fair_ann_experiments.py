9ab65e822f6283e2dd4178bbad80f3409e607c4e
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-14 13:35:12 +0100

    Added dataset preprocessing configuration files and scripts Added configuration files for experiments with datasets and models

---
+++ preprocess_data_for_fair_ann_experiments.py
@@ -0,0 +1,13 @@
+from elliot.run import run_experiment
+
+# start by preprocessing movielens 1M dataset
+print("Done! We are now starting preprocessing of movielens 1M dataset")
+run_experiment("config_files/preprocess_movielens_1M.yml")
+
+# preprocess last fm 1K dataset
+print("Done! We are now starting preprocessing of last fm 1K dataset")
+run_experiment("config_files/preprocess_lastfm_1K.yml")
+
+# preprocess amazon books dataset
+print("Done! We are now starting preprocessing of amazon books dataset")
+run_experiment("config_files/preprocess_amazon_books.yml")
--- sample_fairness_ann.py
+++ sample_fairness_ann.py
@@ -2,4 +2,4 @@
 from elliot.run import run_experiment
 
 print("Done! We are now starting the Fair ANN Elliot's experiment")
-run_experiment("config_files/FairANN_configuration.yml")
+run_experiment("config_files/FairANN_configuration_movielens_100k.yml")
