c13de77971a621903250cc21093170dcb8d10da0
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-23 23:33:24 +0100

    Minor changes to run experiments

--- script_run_experiments_item.py
+++ script_run_experiments_item.py
@@ -3,7 +3,7 @@ from elliot.run import run_experiment
 
 print("Done! We are now starting the Fair ANN Elliot's experiment with Item-based methods")
 
-run_experiment("config_run_experiments/item_experiment_movielens_1m.yml")
+# run_experiment("config_run_experiments/item_experiment_movielens_1m.yml")
 
 run_experiment("config_run_experiments/item_experiment_lastfm_1k.yml")
 
