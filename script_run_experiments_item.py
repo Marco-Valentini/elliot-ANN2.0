0495dc59a4ad0593c82587a7cb854d171cdca1a6
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-23 19:25:23 +0100

    Repeat experiments

--- script_run_experiments_item.py
+++ script_run_experiments_item.py
@@ -3,8 +3,8 @@ from elliot.run import run_experiment
 
 print("Done! We are now starting the Fair ANN Elliot's experiment with Item-based methods")
 
-# run_experiment("config_run_experiments/item_experiment_movielens_1m.yml")
-#
-# run_experiment("config_run_experiments/item_experiment_lastfm_1k.yml")
+run_experiment("config_run_experiments/item_experiment_movielens_1m.yml")
+
+run_experiment("config_run_experiments/item_experiment_lastfm_1k.yml")
 
 run_experiment("config_run_experiments/item_experiment_amazon_books.yml")
