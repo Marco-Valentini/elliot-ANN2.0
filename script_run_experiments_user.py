d8d96392858db0746e868ff711c88c0c0e816887
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-23 19:22:29 +0100

    Minor changes to run experiments on amazon books

--- script_run_experiments_user.py
+++ script_run_experiments_user.py
@@ -2,9 +2,9 @@
 from elliot.run import run_experiment
 
 print("Done! We are now starting the Fair ANN Elliot's experiment with User-based methods")
+#
+# run_experiment("config_run_experiments/user_experiment_movielens_1m.yml")
+#
+# run_experiment("config_run_experiments/user_experiment_lastfm_1k.yml")
 
-run_experiment("config_run_experiments/user_experiment_movielens_1m.yml")
-
-run_experiment("config_run_experiments/user_experiment_lastfm_1k.yml")
-
-# run_experiment("config_run_experiments/user_experiment_amazon_books.yml")
+run_experiment("config_run_experiments/user_experiment_amazon_books.yml")
