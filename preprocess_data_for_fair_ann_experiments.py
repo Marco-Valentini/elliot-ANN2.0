186c87b6a938701ac6bd47416484c95c8e1a4a6b
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-28 20:25:47 +0100

    Run item experiments on last and amazon books

--- preprocess_data_for_fair_ann_experiments.py
+++ preprocess_data_for_fair_ann_experiments.py
@@ -5,9 +5,9 @@ from elliot.run import run_experiment
 # run_experiment("config_preprocess_dataset/preprocess_movielens_1m.yml")
 
 # preprocess last fm 1K dataset
-print("Done! We are now starting preprocessing of last fm 1K dataset")
-run_experiment("config_preprocess_dataset/preprocess_lastfm_1K.yml")
+# print("Done! We are now starting preprocessing of last fm 1K dataset")
+# run_experiment("config_preprocess_dataset/preprocess_lastfm_1K.yml")
 
 # # preprocess amazon books dataset
-# print("Done! We are now starting preprocessing of amazon books dataset")
-# run_experiment("config_preprocess_dataset/preprocess_amazon_books.yml")
+print("Done! We are now starting preprocessing of amazon books dataset")
+run_experiment("config_preprocess_dataset/preprocess_amazon_books.yml")
