7f7264d5d6938d96ce063d6eeb8dec8d192fcaf7
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-30 16:45:48 +0100

    Minor changes to run on ironheaart

--- preprocess_data_for_fair_ann_experiments.py
+++ preprocess_data_for_fair_ann_experiments.py
@@ -1,13 +1,13 @@
 from elliot.run import run_experiment
 
 # # start by preprocessing movielens 1M dataset
-# print("Done! We are now starting preprocessing of movielens 1M dataset")
-# run_experiment("config_preprocess_dataset/preprocess_movielens_1m.yml")
+print("Done! We are now starting preprocessing of movielens 1M dataset")
+run_experiment("config_preprocess_dataset/preprocess_movielens_1m.yml")
 
 # preprocess last fm 1K dataset
-# print("Done! We are now starting preprocessing of last fm 1K dataset")
-# run_experiment("config_preprocess_dataset/preprocess_lastfm_1K.yml")
+print("Done! We are now starting preprocessing of last fm 1K dataset")
+run_experiment("config_preprocess_dataset/preprocess_lastfm_1K.yml")
 
-# # preprocess amazon books dataset
+# preprocess amazon books dataset
 print("Done! We are now starting preprocessing of amazon books dataset")
 run_experiment("config_preprocess_dataset/preprocess_amazon_books.yml")
