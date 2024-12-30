729966f0b6a4de3b66ebb7f7d0b0263c9fad3b54
Marco Valentini <m.valentini7@studenti.poliba.it>
2024-12-30 17:39:46 +0100

    Evaluate Movielens 1m item-based

--- script_evaluate_experiments.py
+++ script_evaluate_experiments.py
@@ -2,17 +2,17 @@
 from elliot.run import run_experiment
 
 print("Done! We are now starting the Evaluation of Fair ANN Elliot's experiment with Item-based methods")
-
-run_experiment("config_compute_metrics/evaluate_item_movielens_1m.yml")
-
-run_experiment("config_compute_metrics/evaluate_item_lastfm_1k.yml")
-
-run_experiment("config_compute_metrics/evaluate_item_amazon_books.yml")
+#
+# run_experiment("config_compute_metrics/evaluate_item_movielens_1m.yml")
+#
+# run_experiment("config_compute_metrics/evaluate_item_lastfm_1k.yml")
+#
+# run_experiment("config_compute_metrics/evaluate_item_amazon_books.yml")
 
 print("Done! We are now starting the Evaluation of Fair ANN Elliot's experiment with User-based methods")
 
 run_experiment("config_compute_metrics/evaluate_user_movielens_1m.yml")
 
-run_experiment("config_compute_metrics/evaluate_user_lastfm_1k.yml")
-
-run_experiment("config_compute_metrics/evaluate_user_amazon_books.yml")
+# run_experiment("config_compute_metrics/evaluate_user_lastfm_1k.yml")
+#
+# run_experiment("config_compute_metrics/evaluate_user_amazon_books.yml")
