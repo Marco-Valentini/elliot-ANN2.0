622bb8cf99777c902767554d6ce6b085b46ecdcd
Marco Valentini <m.valentini7@studenti.poliba.it>
2025-01-07 14:52:01 +0100

    small change to run on master user-based evaluation

--- script_evaluate_experiments.py
+++ script_evaluate_experiments.py
@@ -1,9 +1,9 @@
 
 from elliot.run import run_experiment
 
-print("Done! We are now starting the Evaluation of Fair ANN Elliot's experiment with Item-based methods")
+# print("Done! We are now starting the Evaluation of Fair ANN Elliot's experiment with Item-based methods")
 #
-run_experiment("config_compute_metrics/evaluate_item_movielens_1m.yml")
+# run_experiment("config_compute_metrics/evaluate_item_movielens_1m.yml")
 #
 # run_experiment("config_compute_metrics/evaluate_item_lastfm_1k.yml")
 #
@@ -11,7 +11,7 @@ run_experiment("config_compute_metrics/evaluate_item_movielens_1m.yml")
 
 print("Done! We are now starting the Evaluation of Fair ANN Elliot's experiment with User-based methods")
 
-# run_experiment("config_compute_metrics/evaluate_user_movielens_1m.yml")
+run_experiment("config_compute_metrics/evaluate_user_movielens_1m.yml")
 
 # run_experiment("config_compute_metrics/evaluate_user_lastfm_1k.yml")
 #
