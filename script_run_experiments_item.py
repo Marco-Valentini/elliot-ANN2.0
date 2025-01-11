3d2caf4d6ba54959c67e9ed420c4e1625a477db6
Marco Valentini <m.valentini7@studenti.poliba.it>
2025-01-11 12:26:27 +0100

    Minor changes to run on CINECA for Amazon books

--- script_run_experiments_item.py
+++ script_run_experiments_item.py
@@ -9,6 +9,6 @@ print("Done! We are now starting the Fair ANN Elliot's experiment with Item-base
 
 # run_experiment("config_run_experiments/item_experiment_movielens_1m.yml")
 
-run_experiment("config_run_experiments/item_experiment_lastfm_1k.yml")
+# run_experiment("config_run_experiments/item_experiment_lastfm_1k.yml")
 
-# run_experiment("config_run_experiments/item_experiment_amazon_books.yml")
+run_experiment("config_run_experiments/item_experiment_amazon_books.yml")
