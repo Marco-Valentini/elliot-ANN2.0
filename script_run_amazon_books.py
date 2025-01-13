53630e469f5efa0dd7d15dbd2f89b7f804328a3e
Marco Valentini <m.valentini7@studenti.poliba.it>
2025-01-13 11:51:35 +0100

    Modular script to run batch jobs on CINECA

---
+++ script_run_amazon_books.py
@@ -0,0 +1,22 @@
+
+from elliot.run import run_experiment
+import warnings
+import argparse
+
+# Suppress all warnings
+warnings.filterwarnings("ignore")
+
+# read the dataset
+parser = argparse.ArgumentParser(description="Run a script to run experiments on Amazon Books dataset.")
+parser.add_argument('--type', type=str, default='item', choices=['item', 'user'])
+parser.add_argument('--version', type=int, default=1, choices=[1, 2, 3, 4, 5, 6, 7])
+args = parser.parse_args()
+
+# take the type of experiment the input arguments
+type = args.type
+
+# take the version of the experiment from the input arguments
+version = args.version
+
+print("Done! We are now starting the Fair ANN Elliot's experiment with Amazon Books dataset")
+run_experiment(f"config_run_experiments/{type}_experiment_amazon_books_{version}.yml")
