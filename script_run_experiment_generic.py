f8c5016e0ca1b9aacbd5e766955428f5657c7e31
Marco Valentini <m.valentini7@studenti.poliba.it>
2025-01-17 10:48:39 +0100

    Minor fixes

--- script_run_experiment_generic.py
+++ script_run_experiment_generic.py
@@ -7,7 +7,7 @@ import argparse
 warnings.filterwarnings("ignore")
 
 # read the dataset
-parser = argparse.ArgumentParser(description="Run a script to run experiments on Amazon Books dataset.")
+parser = argparse.ArgumentParser(description="Run a script to run experiments on a generic dataset.")
 parser.add_argument('--type', type=str, default='item', choices=['item', 'user'])
 parser.add_argument('--version', type=str, default='1a')
 parser.add_argument('--dataset', type=str, default='amazon_books', choices=['amazon_books', 'lastfm_1k', 'yelp', 'movielens_100k', 'movielens_1m'])
